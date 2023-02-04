2945630315b040f579bcdd4657dd2a353629823d
Luis Rodriguez <luisrodge@outlook.com>
2023-02-03 23:17:32 -0600

    init

---
+++ components/Layout.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import { ReactNode } from "react";
+import { Inter } from "@next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Layout({ children }: { children: ReactNode }) {
+  return (
+    <div className={inter.className}>
+      <div className="max-w-4xl m-auto py-8 flex gap-12 items-center">
+        <div className="text-zinc-500">
+          <Link href="/">bg removal app</Link>
+        </div>
+        <div className="flex-1 ">
+          <ul className="flex gap-8 justify-end text-zinc-500">
+            <li>
+              <Link href="">Sign In</Link>
+            </li>
+            <li>
+              <Link href="">Get Started</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <main className="max-w-4xl m-auto">{children}</main>
+    </div>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,60 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from "next/head";
+import { useDropzone, FileWithPath } from "react-dropzone";
+import { useMemo } from "react";
 
-const inter = Inter({ subsets: ['latin'] })
+const baseStyle = {
+  flex: 1,
+  display: "flex",
+  flexDirection: "column",
+  padding: "20px",
+  borderWidth: 2,
+  borderRadius: 2,
+  borderColor: "#eeeeee",
+  borderStyle: "dashed",
+  backgroundColor: "#fafafa",
+  color: "#bdbdbd",
+  outline: "none",
+  transition: "border .24s ease-in-out",
+};
+
+const focusedStyle = {
+  borderColor: "#2196f3",
+};
+
+const acceptStyle = {
+  borderColor: "#00e676",
+};
+
+const rejectStyle = {
+  borderColor: "#ff1744",
+};
 
 export default function Home() {
+  const {
+    getRootProps,
+    getInputProps,
+    acceptedFiles,
+    isFocused,
+    isDragAccept,
+    isDragReject,
+  } = useDropzone({ accept: { "image/*": [] } });
+
+  const style = useMemo(
+    () => ({
+      ...baseStyle,
+      ...(isFocused ? focusedStyle : {}),
+      ...(isDragAccept ? acceptStyle : {}),
+      ...(isDragReject ? rejectStyle : {}),
+    }),
+    [isFocused, isDragAccept, isDragReject]
+  );
+
+  const files = acceptedFiles.map((file: FileWithPath) => (
+    <li key={file.path}>
+      {file.path} - {file.size} bytes
+    </li>
+  ));
+
   return (
     <>
       <Head>
@@ -14,110 +63,25 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
+      <div className="mt-12 border-zinc-800 border-b pb-6">
+        <h1 className="text-5xl">Try for free!</h1>
+        <p className="text-zinc-500 pt-2">
+          Remove the background from up to 10 images, no charge.
+        </p>
+      </div>
+      <div className="flex mt-12 gap-12">
+        <div className="flex-1">
+          <h1 className="text-xl mb-4">Upload</h1>
+          <div {...getRootProps({ style: style as React.CSSProperties })}>
+            <input {...getInputProps()} />
+            <p>Drop a image, or click to select one</p>
           </div>
+          <ul className="mt-2">{files}</ul>
         </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
+        <div className="flex-1">
+          <h1 className="text-xl">Result</h1>
         </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+      </div>
     </>
-  )
+  );
 }
