b12a1d765fbd7850dd4986dca48217e539093ce8
Luis Rodriguez <luisrodge@outlook.com>
2023-02-06 19:51:29 -0600

    minor change

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -6,7 +6,7 @@ export default function Dashboard() {
   const { used, limit } = usePackage();
   return (
     <div>
-      <div className="mb-8">
+      <div className="mb-8 mt-4">
         <h2 className="mb-2 text-xl">Credit used</h2>
         <div className="flex items-center gap-4">
           <div className="h-2.5 w-full rounded-full bg-zinc-300">
