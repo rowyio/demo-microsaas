722a3e5d754ccd852e1cb329a9108ca6668afb7a
Luis Rodriguez <luisrodge@outlook.com>
2023-02-07 21:41:02 -0600

    minor change

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,8 +1,16 @@
 import Packages from "@/components/Packages";
 import UsageBar from "@/components/UsageBar";
-import usePackage from "@/hooks/usePackage";
+import useAuth from "@/hooks/useAuth";
+import { useRouter } from "next/router";
 
 export default function Dashboard() {
+  const { loading, user } = useAuth();
+  const router = useRouter();
+
+  if (loading) return null;
+
+  if (!user) router.push("/");
+
   return (
     <div>
       <div className="mb-8 mt-4">
