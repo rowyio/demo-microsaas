a80e90a6b0c9e5c1f8d0ae94e7e351ec3f101680
Luis <luis@Capacitives-MacBook-Pro.local>
2023-03-04 13:59:26 -0600

    minor change

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -5,7 +5,7 @@ import Footer from "./Footer";
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div
-      className={`m-auto flex min-h-screen max-w-6xl flex-col px-4 text-zinc-700 md:px-0`}
+      className={`m-auto flex min-h-screen max-w-5xl flex-col px-4 text-zinc-700 md:px-0`}
     >
       <Header />
       <main className="flex w-full flex-1 flex-col">{children}</main>
