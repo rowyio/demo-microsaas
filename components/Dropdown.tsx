9ea9bbe66e08a9666e0c30ff6e1dc1a432f138e7
Luis <luis@Capacitives-MacBook-Pro.local>
2023-03-04 16:51:54 -0600

    ui changes

---
+++ components/Dropdown.tsx
@@ -0,0 +1,110 @@
+import { userAuthAtom } from "@/atoms";
+import { auth } from "@/lib/firebase";
+import { signOut } from "firebase/auth";
+import { useAtomValue } from "jotai";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect, useRef, useState } from "react";
+
+export default function Dropdown() {
+  const [isOpen, setIsOpen] = useState(false);
+  const { user, isAuthenticated } = useAtomValue(userAuthAtom);
+  const router = useRouter();
+  const dropdownRef = useRef<HTMLDivElement>(null);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleClickOutside = (event: MouseEvent) => {
+    event.target;
+    if (
+      dropdownRef.current &&
+      event.target instanceof HTMLElement &&
+      !dropdownRef.current.contains(event.target)
+    ) {
+      setIsOpen(false);
+    }
+  };
+
+  const onLinkClick = () => {
+    setIsOpen(false);
+    toggleDropdown();
+  };
+
+  const logout = () => {
+    signOut(auth)
+      .then(() => {
+        // Sign-out successful.
+        console.log("Signed out successfully");
+        // location.replace("/");
+        router.push("/");
+      })
+      .catch((error) => {
+        // An error happened.
+        console.log("Signed out failed", error);
+      });
+  };
+
+  useEffect(() => {
+    document.addEventListener("click", handleClickOutside, true);
+    return () => {
+      document.removeEventListener("click", handleClickOutside, true);
+    };
+  }, []);
+
+  return (
+    <div className="relative z-50" ref={dropdownRef}>
+      <button
+        className="flex items-center text-white focus:outline-none"
+        onClick={toggleDropdown}
+      >
+        {user && (
+          <Image
+            src={user.photoUrl ? user.photoUrl : "avatar.png"}
+            alt="user photo"
+            width={43}
+            height={43}
+            className="rounded-full"
+          />
+        )}
+        <svg
+          className="ml-1 h-4 w-4 fill-black"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M10 12l-5.79-5.79a1 1 0 0 1 1.41-1.41L10 9.17l4.38-4.38a1 1 0 0 1 1.41 1.41L10 12z" />
+        </svg>
+      </button>
+      {isOpen && (
+        <div className="absolute right-0 mt-2 w-36 rounded-lg bg-white py-2 shadow-xl md:left-0">
+          <Link
+            href="dashboard"
+            className="block px-4 py-2 text-gray-800 hover:bg-black hover:text-white"
+            onClick={onLinkClick}
+          >
+            Dashboard
+          </Link>
+          <Link
+            href="packages"
+            className="block px-4 py-2 text-gray-800 hover:bg-black hover:text-white"
+            onClick={onLinkClick}
+          >
+            Packages
+          </Link>
+          <a
+            href="#"
+            className="block px-4 py-2 text-gray-800 hover:bg-black hover:text-white"
+            onClick={() => {
+              logout();
+              onLinkClick();
+            }}
+          >
+            Sign out
+          </a>
+        </div>
+      )}
+    </div>
+  );
+}
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,76 +1,39 @@
 import Link from "next/link";
-import { signOut } from "firebase/auth";
-import { auth } from "@/lib/firebase";
 import { registerOrLogin } from "@/lib/auth";
 import { useAtomValue } from "jotai";
 import { userAuthAtom } from "@/atoms";
 import Image from "next/image";
-import { useRouter } from "next/router";
-import { useEffect, useRef, useState } from "react";
-import { Home, Home2, PhotoEdit } from "tabler-icons-react";
+import { Home2, PhotoEdit } from "tabler-icons-react";
+import Dropdown from "./Dropdown";
 
 export default function Header() {
-  const [isOpen, setIsOpen] = useState(false);
-  const { user, isAuthenticated } = useAtomValue(userAuthAtom);
-  const router = useRouter();
-  const dropdownRef = useRef<HTMLDivElement>(null);
-
-  const toggleDropdown = () => {
-    setIsOpen(!isOpen);
-  };
-
-  const handleClickOutside = (event: MouseEvent) => {
-    event.target;
-    if (
-      dropdownRef.current &&
-      event.target instanceof HTMLElement &&
-      !dropdownRef.current.contains(event.target)
-    ) {
-      setIsOpen(false);
-    }
-  };
-
-  const logout = () => {
-    signOut(auth)
-      .then(() => {
-        // Sign-out successful.
-        console.log("Signed out successfully");
-        // location.replace("/");
-        router.push("/");
-      })
-      .catch((error) => {
-        // An error happened.
-        console.log("Signed out failed", error);
-      });
-  };
-
-  useEffect(() => {
-    document.addEventListener("click", handleClickOutside, true);
-    return () => {
-      document.removeEventListener("click", handleClickOutside, true);
-    };
-  }, []);
+  const { isAuthenticated } = useAtomValue(userAuthAtom);
 
   return (
     <div
       className={`block items-center gap-12 border-b border-b-zinc-200 py-8 md:flex md:border-b-0`}
     >
       <div className="mb-5 text-center text-lg md:mb-0">
-        <Link
-          href="/"
-          className="flex items-center justify-center space-x-2 sm:justify-start "
-        >
-          <Image
-            alt="header logo"
-            src="/photo.svg"
-            className="h-7 w-7 sm:h-8 sm:w-8"
-            width={24}
-            height={24}
-          />
-          <h1 className="ml-2 text-xl tracking-tight">
-            background removal app
-          </h1>
-        </Link>
+        <div className="flex justify-center gap-4">
+          <Link
+            href="/"
+            className="flex items-center justify-center space-x-2 sm:justify-start "
+          >
+            <Image
+              alt="header logo"
+              src="/photo.svg"
+              className="h-7 w-7 sm:h-8 sm:w-8"
+              width={24}
+              height={24}
+            />
+            <h1 className="ml-2 text-xl tracking-tight">
+              background removal app
+            </h1>
+          </Link>
+          <div className="md:hidden">
+            <Dropdown />
+          </div>
+        </div>
       </div>
       <div className="block flex-1">
         <ul className="flex items-center justify-center gap-5 text-lg text-zinc-600 md:justify-end">
@@ -106,7 +69,13 @@ export default function Header() {
               </li>
               <li>
                 <Link href="/remove">
-                  <button className="flex cursor-pointer gap-1 py-2 px-2 hover:text-black active:bg-zinc-200">
+                  <button className="hidden cursor-pointer gap-1 py-2 px-2 hover:text-black active:bg-zinc-200 sm:flex">
+                    <PhotoEdit />
+                    Remove Background
+                  </button>
+                </Link>
+                <Link href="/remove">
+                  <button className="flex cursor-pointer items-center gap-1 rounded-md bg-black py-1 px-4 text-sm text-white hover:text-zinc-300 sm:hidden">
                     <PhotoEdit />
                     Remove Background
                   </button>
@@ -115,53 +84,8 @@ export default function Header() {
             </>
           )}
           {isAuthenticated && (
-            <li>
-              <div className="relative" ref={dropdownRef}>
-                <button
-                  className="flex items-center text-white focus:outline-none"
-                  onClick={toggleDropdown}
-                >
-                  {user && (
-                    <Image
-                      src={user.photoUrl ? user.photoUrl : "avatar.png"}
-                      alt="user photo"
-                      width={43}
-                      height={43}
-                      className="rounded-full"
-                    />
-                  )}
-                  <svg
-                    className="ml-1 h-4 w-4 fill-black"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path d="M10 12l-5.79-5.79a1 1 0 0 1 1.41-1.41L10 9.17l4.38-4.38a1 1 0 0 1 1.41 1.41L10 12z" />
-                  </svg>
-                </button>
-                {isOpen && (
-                  <div className="absolute right-0 mt-2 w-36 rounded-lg bg-white py-2 shadow-xl md:left-0">
-                    <Link
-                      href="dashboard"
-                      className="block px-4 py-2 text-gray-800 hover:bg-black hover:text-white"
-                    >
-                      Dashboard
-                    </Link>
-                    <Link
-                      href="packages"
-                      className="block px-4 py-2 text-gray-800 hover:bg-black hover:text-white"
-                    >
-                      Packages
-                    </Link>
-                    <a
-                      href="#"
-                      className="block px-4 py-2 text-gray-800 hover:bg-black hover:text-white"
-                      onClick={logout}
-                    >
-                      Sign out
-                    </a>
-                  </div>
-                )}
-              </div>
+            <li className="hidden sm:block">
+              <Dropdown />
             </li>
           )}
         </ul>
